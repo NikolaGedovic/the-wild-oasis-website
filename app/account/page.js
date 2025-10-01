5ffe24f85f312808ea211090253af5630ba2eb5e
Nikola Gedović <nikola.gedovic@gmail.com>
2025-10-01 16:32:00 +0200

    building a custom sign in page

--- app/account/page.js
+++ app/account/page.js
@@ -1,11 +1,14 @@
+import { auth } from "../_lib/auth";
+
 export const metadata = {
   title: "Guest area",
 };
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth();
   return (
     <h2 className="text-2xl font-semibold text-accent-400 mb-7">
-      Welcome, Nikola
+      Welcome, {session.user.name}
     </h2>
   );
 }
