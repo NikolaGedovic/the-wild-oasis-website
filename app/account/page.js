e7f24b8bf74227cfe34eb306fd3467b5390adc48
Nikola Gedović <nikola.gedovic@gmail.com>
2025-10-01 16:58:42 +0200

    creating a new guest on first sign in

--- app/account/page.js
+++ app/account/page.js
@@ -6,6 +6,7 @@ export const metadata = {
 
 export default async function Page() {
   const session = await auth();
+
   return (
     <h2 className="text-2xl font-semibold text-accent-400 mb-7">
       Welcome, {session.user.name}
