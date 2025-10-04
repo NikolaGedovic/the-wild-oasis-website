ea917f2ccd8a24a71ad02e0c1077d23ea84a85af
Nikola Gedović <nikola.gedovic@gmail.com>
2025-10-04 17:16:57 +0200

    deleting a reservation

--- app/_components/DeleteReservation.js
+++ app/_components/DeleteReservation.js
@@ -1,8 +1,14 @@
+"use client";
+
 import { TrashIcon } from "@heroicons/react/24/solid";
+import { deleteReservation } from "../_lib/actions";
 
 function DeleteReservation({ bookingId }) {
   return (
-    <button className="flex items-center flex-grow gap-2 px-3 text-xs font-bold uppercase transition-colors group text-primary-300 hover:bg-accent-600 hover:text-primary-900">
+    <button
+      onClick={() => deleteReservation(bookingId)}
+      className="flex items-center flex-grow gap-2 px-3 text-xs font-bold uppercase transition-colors group text-primary-300 hover:bg-accent-600 hover:text-primary-900"
+    >
       <TrashIcon className="w-5 h-5 transition-colors text-primary-600 group-hover:text-primary-800" />
       <span className="mt-1">Delete</span>
     </button>
