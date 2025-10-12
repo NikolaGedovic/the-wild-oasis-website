1c480b9f980ed03b18bee7d9b31a0129b9a85449
Nikola Gedović <nikola.gedovic@gmail.com>
2025-10-12 16:53:01 +0200

    making homepage main heading smaller on phones

--- app/page.js
+++ app/page.js
@@ -15,9 +15,10 @@ export default function Page() {
       />
 
       <div className="relative z-10 text-center">
-        <h1 className="mb-10 font-normal tracking-tight text-8xl text-primary-50">
+        <h1 className="mb-10 text-5xl font-normal tracking-tight sm:text-7xl lg:text-8xl text-primary-50">
           Welcome to paradise.
         </h1>
+
         <Link
           href="/cabins"
           className="px-8 py-6 text-lg font-semibold transition-all bg-accent-500 text-primary-800 hover:bg-accent-600"
